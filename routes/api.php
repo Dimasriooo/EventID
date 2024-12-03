04809c748bbbedfed702d5547cb3ad959eb2d7cd
Dimas <dimas123@gmail.com>
2024-12-03 17:13:07 +0700

    penambahan auth

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,22 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\PackageController;
+use App\Http\Controllers\PackageFeaturesController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+
+Route::post("/register", [AuthController::class, 
+"register"]);
+Route::post("/login", [AuthController::class, 
+"login"]);
+
 
 Route::middleware('api')->group(function () {
     Route::resource('bookings', BookingController::class);
     Route::resource('users', UserController::class);
     Route::resource('packages', PackageController::class);
+    Route::resource('packagesFeatures', PackageFeaturesController::class);
 });
 
