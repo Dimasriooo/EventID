e7267b4708467b6eb4435203587994681beaea95
Dimas <114458950+Dimasriooo@users.noreply.github.com>
2024-12-27 16:22:15 +0700

    Update Final

--- resources/views/pages/events/packagesfeatures.blade.php
+++ resources/views/pages/events/packagesfeatures.blade.php
@@ -2,10 +2,7 @@
 
 
 @section('header')
-    <h1 class="mt-4">Dashboard</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Dashboard</li>
-    </ol>
+    <h1 class="mt-4">Dashboard Package Featured</h1>
 @endsection
 
 @section('content')
