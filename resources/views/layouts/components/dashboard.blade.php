d651eace825115e538f9129f25b7a3de92a82218
Dimas <114458950+Dimasriooo@users.noreply.github.com>
2024-12-30 03:05:30 +0700

    Update Final dashboard

--- resources/views/layouts/components/dashboard.blade.php
+++ resources/views/layouts/components/dashboard.blade.php
@@ -35,7 +35,7 @@
         </div>
         <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">
-                <div class="card-body">packagesFeatures</div>
+                <div class="card-body">packagesFeatured</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="/Features">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -1,49 +1,8 @@
-{{-- @php
-    $menus = [
-        (object) [
-        'title' => '',
-        'icon' => 'fas fa-search fa-fw',
-        'path' => '/',
-        ],
-        (object) [
-        'title' => '',
-        'icon' => 'fas fa-search fa-fw',
-        'path' => '/packages',
-        ],
-        (object) [
-        'title' => '',
-        'icon' => 'fas fa-search fa-fw',
-        'path' => '/Bookings',
-        ],
-    ];
-    $bookings = [
-        (object) [
-        'title' => 'Booking',
-        'icon' => 'fas fa-search fa-fw',
-        'path' => '/Bookings',
-        ],
-    ];
-
-        
-        
-@endphp --}}
-
-
-
 <div id="layoutSidenav_nav">
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Dashboard</div>
-                {{-- @foreach ($menus as $menu )  --}}
-                {{-- <div class="sb-sidenav-menu-heading"></div>
-                <a class="nav-link" href="{{ $menu->path }}">
-                    <i class="sb-nav-link-icon" {{ $menu->icon }}></i>
-                    <p>
-                        {{ $menu->title }}
-                    </p>
-                </a> --}}
-                {{-- @endforeach --}}
                 <a class="nav-link" href="/">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-house"></i></div>
                     Dashboard
@@ -55,14 +14,14 @@
                 </a>
                 <a class="nav-link" href="/Features">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    packagesFeatures
+                    packagesFeatured
                 </a>
-                {{-- @foreach  ($bookings as $booking ) --}}
+               
                 <a class="nav-link" href="/Bookings">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-bell"></i></i></div>
                     booking
                 </a>
-                {{-- @endforeach --}}
+                
                 
             </div>
         </div>
